659486559f91e496cd8bc0f8c266368f857a71a2
Ramirez Vargas, José Pablo <webJose@gmail.com>
2024-05-19 19:31:20 -0600

    feat:  Export the WjDataViewTheme component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,4 @@
 export * from "./WjDataView.svelte";
 export { default as WjDataView } from "./WjDataView.svelte";
+export * from "./WjDataViewTheme.svelte";
+export { default as WjDataViewTheme } from "./WjDataViewTheme.svelte";
